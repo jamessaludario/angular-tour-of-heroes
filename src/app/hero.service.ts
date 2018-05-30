4559bc084c3377417efd41b4a4c3cee5f77740d9
James Saludario <jsaludario12@gmail.com>
2018-05-30 11:23:21 +0800

    Modified Hero details

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import {MessageService} from "./message.service";
-import { Observable } from 'rxjs';
-import {of} from "rxjs/observable/of";
+import {Observable, of} from 'rxjs';
 
 @Injectable()
 
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 21, name: 'Batman '},
+  { id: 22, name: 'Superman' },
+  { id: 23, name: 'Lastickman' },
+  { id: 24, name: 'Bomberman' },
+  { id: 25, name: 'Hugh Jackman' },
+  { id: 26, name: 'Puto Suman' },
+  { id: 27, name: 'Gulaman' },
+  { id: 28, name: 'Palaman' },
+  { id: 29, name: 'Gidaman' },
+  { id: 30, name: 'Gunman' }
+];
