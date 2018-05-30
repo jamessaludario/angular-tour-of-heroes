b651a54843cfa95925919c6b15099cb65cf63186
James Saludario <jsaludario12@gmail.com>
2018-05-30 13:07:33 +0800

    routing session

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import {HeroService} from "../hero.service";
 
 
@@ -10,23 +9,20 @@ import {HeroService} from "../hero.service";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  heroes = HEROES;
-
-  selectedHero: Hero;
-
+  heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
-
-  getHeroes(): void{
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
 
   ngOnInit() {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
