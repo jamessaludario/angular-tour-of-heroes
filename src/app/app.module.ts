8fb080ed274d849ee96f1a3e569d8d89db1deb3c
James Saludario <jsaludario12@gmail.com>
2018-05-31 09:13:50 +0800

    HTTP Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
-import { HttpClientModule }    from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
 import { AppRoutingModule }     from './app-routing.module';
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
@@ -13,6 +10,9 @@ import { HeroSearchComponent }  from './hero-search/hero-search.component';
 import { MessagesComponent }    from './messages/messages.component';
 import {HeroService} from './hero.service';
 import {MessageService} from './message.service';
+import {HttpClientModule} from '@angular/common/http';
+
+
 
 
 @NgModule({
@@ -22,9 +22,6 @@ import {MessageService} from './message.service';
     AppRoutingModule,
     HttpClientModule,
 
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
   ],
   declarations: [
     AppComponent,
